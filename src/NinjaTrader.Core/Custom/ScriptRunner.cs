1fcc58a1d27414a887df27daf4013632efc39f70
Boris Turk <boris.turk@xlab.si>
2022-10-02 13:55:03 +0200

    Implementing trading framework.

--- src/NinjaTrader.Core/Custom/ScriptRunner.cs
+++ src/NinjaTrader.Core/Custom/ScriptRunner.cs
@@ -27,10 +27,15 @@ namespace NinjaTrader.Core.Custom
 
         private void ThrowIfPreconditionsViolated()
         {
-            if (Start == DateTime.MinValue) throw new InvalidOperationException("Start not defined");
-            if (End == DateTime.MinValue) throw new InvalidOperationException("Start not defined");
-            if (Start >= DateTime.Now) throw new InvalidOperationException("Start cannot be in future.");
-            if (Start >= End) throw new InvalidOperationException("Start must be smaller than End period.");
+            if (Start == DateTime.MinValue) throw CreateException("{0} not defined", nameof(Start));
+            if (End == DateTime.MinValue) throw CreateException("{0} not defined", nameof(End));
+            if (Start >= DateTime.Now) throw CreateException("{0} cannot be in future.", nameof(Start));
+            if (Start >= End) throw CreateException("{0} must be smaller than ${1}.", nameof(Start), nameof(End));
+        }
+
+        private Exception CreateException(string message, params object[] variableName)
+        {
+            return  new InvalidOperationException(string.Format(message, variableName));
         }
     }
 }
--- src/NinjaTrader.Custom.UnitTests/ScriptRunnerTests.cs
+++ src/NinjaTrader.Custom.UnitTests/ScriptRunnerTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using FluentAssertions;
 using NinjaTrader.Core.Custom;
 using NinjaTrader.Data;
 using NinjaTrader.NinjaScript.Strategies;
@@ -8,6 +9,67 @@ namespace NinjaTrader.Custom.UnitTests
 {
     public class ScriptRunnerTests
     {
+        [Fact]
+        public void Run_StartUndefined_FailsWithExpectedException()
+        {
+            // Arrange
+            var strategy = new ScriptRunnerTestStrategy();
+            var scriptRunner = new ScriptRunner(strategy);
+
+            // Act
+            Action runAction = () => scriptRunner.Run();
+
+            // Assert
+            runAction.Should().Throw<InvalidOperationException>();
+        }
+
+        [Fact]
+        public void Run_EndUndefined_FailsWithExpectedException()
+        {
+            // Arrange
+            var strategy = new ScriptRunnerTestStrategy();
+            var scriptRunner = new ScriptRunner(strategy) { Start = DateTime.Now.AddDays(-1) };
+
+            // Act
+            Action runAction = () => scriptRunner.Run();
+
+            // Assert
+            runAction.Should().Throw<InvalidOperationException>();
+        }
+
+        [Fact]
+        public void Run_StartInFuture_FailsWithExpectedException()
+        {
+            // Arrange
+            var futureStart = DateTime.Now.AddDays(1);
+            var strategy = new ScriptRunnerTestStrategy();
+            var scriptRunner = new ScriptRunner(strategy) { Start = futureStart, End = futureStart.AddDays(1) };
+
+            // Act
+            Action runAction = () => scriptRunner.Run();
+
+            // Assert
+            runAction.Should().Throw<InvalidOperationException>();
+        }
+
+        [Fact]
+        public void Run_StartBiggerThanEnd_FailsWithExpectedException()
+        {
+            // Arrange
+            var strategy = new ScriptRunnerTestStrategy();
+            var scriptRunner = new ScriptRunner(strategy)
+            {
+                Start = DateTime.Now.AddDays(-1),
+                End = DateTime.Now.AddDays(-2)
+            };
+
+            // Act
+            Action runAction = () => scriptRunner.Run();
+
+            // Assert
+            runAction.Should().Throw<InvalidOperationException>();
+        }
+
         [Fact]
         public void Test()
         {
